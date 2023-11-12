21f268dde1df925b7e10a0c34919dea8f1645824
Aleksandr Ataev <onlyaa94@mail.ru>
2023-11-12 13:19:36 +0300

    Initial Commit

---
+++ CalcHelper/CalcHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcHelperApp.swift
+//  CalcHelper
+//
+//  Created by Aleksandr Ataev on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalcHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
